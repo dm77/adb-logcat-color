2ad47851bab751d3bf3531ca14b8b53dfe8cf66f
Dushyanth Maguluru <dushyanth.maguluru@gmail.com>
2011-11-22 02:56:55 -0700

    First commit

---
+++ adb-logcat-color.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+require 'smart_colored/extend'
+
+patterns = {"V/" => {:fg => "black", :bg => "blue",    :label => "[ VERBOSE ]"},
+            "D/" => {:fg => "black", :bg => "cyan",    :label => "[  DEBUG  ]"},
+            "I/" => {:fg => "black", :bg => "cyan",    :label => "[  INFO   ]"},
+            "W/" => {:fg => "black", :bg => "yellow",  :label => "[ WARNING ]"},
+            "E/" => {:fg => "black", :bg => "red",     :label => "[  ERROR  ]"},
+            "F/" => {:fg => "black", :bg => "magenta", :label => "[  FATAL  ]"},
+            "S/" => {:fg => "black", :bg => "white",   :label => "[  SILENT ]"}}
+
+
+IO.popen("adb logcat") do |f|
+  while line = f.gets
+    line.sub!(/\(\s*\d+\)/, "")
+    line.gsub!("\r\n", "")
+
+    match = patterns.keys.find do |x|
+      line.start_with?(x)
+    end
+
+    if match
+      line.sub!(match, " => ")
+      label = patterns[match][:label]
+      fg = patterns[match][:fg]
+      bg = patterns[match][:bg]
+      print label.send("#{fg}_on_#{bg}")
+      puts line.send("#{bg}_on_#{fg}")
+    end
+  end
+end
